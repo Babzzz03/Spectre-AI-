3272f4a86769eba99e45a8ebdf56a3d9dcc59d10
unknown <babawale.emmanuel10@gmail.com>
2022-12-29 02:13:22 +0100

    better

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://spectre-efy9.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
